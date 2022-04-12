a656e0d754358dfb335b2ea4c7fd4b2b28117651
Nora <d240803@gmail.com>
2022-04-12 22:31:40 +0300

    newproject

---
+++ App.js
@@ -0,0 +1,31 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+} from 'react-native';
+
+
+
+
+const App = () => {
+  
+  
+
+  return (
+    <SafeAreaView>
+       <Text>123</Text>
+    </SafeAreaView>
+  );
+};
+
+
+
+export default App;
