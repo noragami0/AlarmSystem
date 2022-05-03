ee22acaa0b007163a867608c55d7dbaf795a99ac
Artem <artem.marchuk@inmost.pro>
2022-05-03 23:10:55 +0300

    fix: fix shadows ios

--- src/utils/res/globalStyles.js
+++ src/utils/res/globalStyles.js
@@ -7,9 +7,10 @@ export default StyleSheet.create({
         padding: 17,
         borderRadius: 10,
         shadowColor: ColorR.BLACK,
+        shadowOpacity: 0.10,
         shadowOffset: {
-            width: 0,
-            height: 2,
+            width: 10,
+            height: 5,
         },
         shadowRadius: 5,
         elevation: 5,
