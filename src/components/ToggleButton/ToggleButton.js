5a23ba2b140c65af8f1228d373a03fc8befefe51
Vognistuy <volegv08903@gmail.com>
2022-05-06 13:22:52 +0300

    fix: ToggleButton

--- src/components/ToggleButton/ToggleButton.js
+++ src/components/ToggleButton/ToggleButton.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {
-    Switch, Text, View,
+    Text, View,
 } from 'react-native';
+import {Switch} from 'react-native-switch';
 import {ColorR} from '../../utils/res/theme';
 import {AppFont} from '../../utils/res/fonts';
 import GlobalStyles from '../../utils/res/globalStyles';
@@ -11,10 +12,22 @@ export default function ToggleButton({isEnabled, onChange, text}) {
         <View style={GlobalStyles.buttonComponent}>
             <Text style={AppFont(15, 600, ColorR.BLACK)}>{text}</Text>
             <Switch
-                trackColor={{false: ColorR.GREY, true: ColorR.BLUE}}
-                thumbColor={ColorR.WHITE}
-                onValueChange={onChange}
+                circleSize={24}
+                barHeight={26}
+                barWidth={60}
+                backgroundActive={ColorR.BLUE}
+                backgroundInactive={ColorR.GREY}
+                changeValueImmediately={true}
+                innerCircleStyle={{alignItems: 'center', justifyContent: 'center', borderColor: ColorR.WHITE}}
+                renderActiveText={false}
+                renderInActiveText={false}
+                switchLeftPx={2}
+                switchRightPx={2}
+                switchWidthMultiplier={2.3}
+                switchBorderRadius={24}
+                onValueChange={(val) => onChange(val)}
                 value={isEnabled}
+                disabled={false}
             />
         </View>
     );
