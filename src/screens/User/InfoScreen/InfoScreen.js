30dbbcd39ebcc53c8a1b0d070cb95ed4852a6cd3
noragami0 <d240803@gmail.com>
2022-05-09 17:22:33 +0300

    feat: add Info Screen

--- src/screens/User/InfoScreen/InfoScreen.js
+++ src/screens/User/InfoScreen/InfoScreen.js
@@ -1,19 +1,25 @@
 import React from 'react';
-import {View} from 'react-native';
+import {Text, View} from 'react-native';
 import ButtonWithArrow from '../../../components/ButtonWithArrow/ButtonWithArrow';
 import ButtonWithIcon from '../../../components/ButtonWithIcon/ButtonWithIcon';
 import {IconsR} from '../../../utils/res/icons';
 import {ColorR} from '../../../utils/res/theme';
 import {MockData} from '../../../utils/mockData';
 import {localize} from '../../../utils/localize/localize';
-import Titles from '../../../components/Titles/Titles';
+// import {AppFont} from '../../../utils/res/fonts';
 
 function InfoScreen() {
     return (
         <View>
-            <Titles text={localize.general.moreInfo} />
+            <View>
+                <Text>
+                    {localize.general.moreInfo}
+                </Text>
+            </View>
             <ButtonWithArrow text={localize.general.share} onClick={() => null} />
-            <Titles text={localize.general.techSupport} />
+            <Text>
+                {localize.general.techSupport}
+            </Text>
             <ButtonWithIcon
                 selectedIcon={IconsR.EMAIL_ICON}
                 iconColor={ColorR.GREY}
