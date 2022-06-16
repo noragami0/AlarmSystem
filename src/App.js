5dadb816b60714ef39f0620cc18ec7771aacad37
Artem <artem.marchuk@inmost.pro>
2022-06-16 23:06:11 +0300

    fix: fix token processing

--- src/App.js
+++ src/App.js
@@ -8,21 +8,20 @@ import {UserHttp} from './utils/http/userHttp';
 
 function App() {
     useEffect(() => {
+        SplashScreen.hide();
+
         const manageTokens = async () => {
-            const region = await AlertLocationHttp.getUserRegion(45.247056, 34.203245);
-            console.log(region);
-            // const token = await Firebase.getToken();
-            const token = 'temperorayFcmTOKEN11rrrrrr1239081231234`1klaKSDJFLAKSJDFLKAJSDFladfalksdjflkasjdfasdf23`123`123';
+            if (await Storage.get(STORAGE_KEYS.REFRESH_TOKEN)) {
+                return;
+            }
+            const token = await Firebase.getToken();
             const response = await UserHttp.login(token);
-            console.log(response);
             const {accessToken, refreshToken} = response.tokens;
 
             await Storage.store(STORAGE_KEYS.REFRESH_TOKEN, refreshToken);
             await Storage.store(STORAGE_KEYS.FCM_TOKEN, token);
             await Storage.store(STORAGE_KEYS.ACCESS_TOKEN, accessToken);
         };
-
-        manageTokens();
 
         const manageRegion = async () => {
             const region = await Storage.get(STORAGE_KEYS.REGION);
@@ -31,9 +30,13 @@ function App() {
             const response = await AlertLocationHttp.updateRegion(currRegion);
             await Storage.store('region', response.region);
         };
-        manageRegion();
 
-        SplashScreen.hide();
+        const startApp = async () => {
+            await manageTokens();
+            await manageRegion();
+        };
+
+        startApp();
     }, []);
 
     return (
