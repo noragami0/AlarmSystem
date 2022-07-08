11a90a59f00f0fdcd1c1698f3e8d65302f9e5206
Artem <artem.marchuk@inmost.pro>
2022-07-08 23:34:42 +0300

    fear: custom sound android

--- android/app/src/main/java/com/allertapp/MainActivity.java
+++ android/app/src/main/java/com/allertapp/MainActivity.java
@@ -1,14 +1,43 @@
 package com.allertapp;
 
+import android.app.NotificationChannel;
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import org.devio.rn.splashscreen.SplashScreen;
 
+import android.app.NotificationManager;
+import android.content.ContentResolver;
+import android.media.AudioAttributes;
+import android.net.Uri;
+import android.os.Build;
+import androidx.core.app.NotificationCompat;
+
+
 public class MainActivity extends ReactActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      String[] sounds = {"morska", "sound1"};
+      for (String sound : sounds) {
+        NotificationChannel notificationChannel = new NotificationChannel(sound, "Alert App", NotificationManager.IMPORTANCE_HIGH);
+        notificationChannel.setShowBadge(true);
+        notificationChannel.setDescription("");
+        AudioAttributes att = new AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_NOTIFICATION)
+                .setContentType(AudioAttributes.CONTENT_TYPE_SPEECH)
+                .
+                .build();
+        notificationChannel.setSound(Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + getPackageName() + "/raw/" + sound), att);
+        notificationChannel.enableVibration(true);
+        notificationChannel.setVibrationPattern(new long[]{400, 400});
+        notificationChannel.setLockscreenVisibility(NotificationCompat.VISIBILITY_PUBLIC);
+        NotificationManager manager = getSystemService(NotificationManager.class);
+        manager.createNotificationChannel(notificationChannel);
+      }
+    }
+
       SplashScreen.show(this);  // here
       super.onCreate(savedInstanceState);
   }
